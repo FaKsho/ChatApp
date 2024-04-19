429d2c860913ab8746cd27bf80a5c43f969392a0
faksho <faksho@proton.me>
2024-04-19 15:52:44 -0300

    connection fixes

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -10,49 +10,50 @@ public class AppLauncher extends GenericFrame {
 
     public static void main(String[] args) { AppLauncher appLauncher = new AppLauncher(); }
 
+    // FRAME CONFIG
     AppLauncher() {
 
         setTitle("Starter");
+        setSize(470,280);
+        setLocationRelativeTo(null);
 
-        add(new StarterPanel());
+        add(new LauncherPanel());
 
         setVisible(true);
     }
 
-    class StarterPanel extends JPanel {
+    // PANEL CONFIG
+    class LauncherPanel extends JPanel implements ActionListener {
 
         JButton server, client;
 
-        StarterPanel() {
+        LauncherPanel() {
 
             server = new JButton("Server");
-            server.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-
-                    dispose();
-
-                    new Server();
-                }
-            });
+            server.addActionListener(this);
 
             client = new JButton("Client");
-            client.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-
-                    dispose();
-
-                    // TODO provisional
-                    String serverIP = JOptionPane.showInputDialog("Ingrese la IP del servidor");
-                    int serverPort = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el puerto del servidor"));
-
-                    new Client(serverIP, serverPort);
-                }
-            });
+            client.addActionListener(this);
 
             add(server);
             add(client);
+        }
+
+        public void actionPerformed(ActionEvent e){
+
+            dispose();
+
+            if(e.getActionCommand().equals("Client")){
+
+                // TODO provisional
+                String serverIP = JOptionPane.showInputDialog("Ingrese la IP del servidor");
+                int serverPort = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el puerto del servidor"));
+
+                new Client(serverIP, serverPort);
+            } else {
+
+                new Server();
+            }
         }
     }
 }
--- src/client/Client.java
+++ src/client/Client.java
@@ -17,22 +17,111 @@ import java.util.ArrayList;
 
 public class Client extends GenericFrame {
 
-    private final String serverIp;
-    private final int serverPort;
-
     public Client(String serverIp, int serverPort) {
 
         // Window Setup
-        add(new ClientPanel());
+        ClientPanel clientPanel = new ClientPanel(serverIp, serverPort);
+        clientPanel.sentOnlineSignal();
+
+        add(clientPanel);
         setTitle("Client");
         setVisible(true);
 
-        //
+    }
+
+}
+class ClientPanel extends JPanel implements Runnable {
+
+    private JTextField message;
+    private JComboBox ip;
+    private JLabel nick;
+    private JTextArea textArea;
+    private JButton sendButton;
+    private JLabel cliente;
+
+    private final String serverIp;
+    private final int serverPort;
+
+    ClientPanel(String serverIp, int serverPort) {
+
         this.serverIp = serverIp;
         this.serverPort = serverPort;
 
+        //setLayout(new BorderLayout());
 
-        sentOnlineSignal();
+        // ELEMENTOS TOP
+        nick = new JLabel();
+        nick.setText(JOptionPane.showInputDialog("Ingresa tu nick"));
+
+        add(nick);
+
+        cliente = new JLabel(" Online ->");
+        add(cliente);
+
+        ip = new JComboBox();
+
+        add(ip);
+
+        // MENSAJES
+        textArea = new JTextArea(14,24);
+        try {
+            textArea.append("YOUR IP:" + InetAddress.getLocalHost() + "\n");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        textArea.setEditable(false);
+        add(textArea);
+
+        // ENVÍO
+        message = new JTextField(20);
+        add(message);
+
+        sendButton = new JButton("Enviar");
+
+        // SALIDA DE DATOS
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if(!message.getText().equals(" online")) textArea.append("You >> " + message.getText() + "\n");
+
+
+                try {
+                    // Creación de socket
+                    Socket socket = new Socket(serverIp, serverPort);
+
+                    PackageDataSend data = new PackageDataSend();
+
+                    data.setNick(nick.getText());
+                    data.setIp(ip.getSelectedItem().toString());
+                    data.setMessage(message.getText());
+
+                    ObjectOutputStream flujoSalida = new ObjectOutputStream(socket.getOutputStream());
+
+                    flujoSalida.writeObject(data);
+
+                    // TODO escribir en el chat el mensaje enviado por el propio cliente
+                    textArea.append(message.getText());
+
+                    socket.close();
+
+                } catch (IOException ex) {
+
+                    throw new RuntimeException(ex);
+                } finally {
+
+
+                    message.setText("");
+                }
+
+            }
+        });
+
+        add(sendButton);
+
+        Thread mihilo = new Thread(this);
+
+        mihilo.start();
     }
 
     void sentOnlineSignal() {
@@ -55,136 +144,45 @@ public class Client extends GenericFrame {
 
     }
 
-    class ClientPanel extends JPanel implements Runnable {
+    @Override
+    public void run() {
 
-        private JTextField message;
-        private JComboBox ip;
-        private JLabel nick;
-        private JTextArea textArea;
-        private JButton sendButton;
-        private JLabel cliente;
+        try {
 
-        ClientPanel() {
+            ServerSocket clientListener = new ServerSocket(serverPort);
 
-            //setLayout(new BorderLayout());
+            Socket cliente;
 
-            // ELEMENTOS TOP
-            nick = new JLabel();
-            nick.setText(JOptionPane.showInputDialog("Ingresa tu nick"));
+            PackageDataSend recievedData;
 
-            add(nick);
+            for(;;){
 
-            cliente = new JLabel(" Online ->");
-            add(cliente);
+                cliente = clientListener.accept();
 
-            ip = new JComboBox();
+                ObjectInputStream inputStream = new ObjectInputStream(cliente.getInputStream());
 
-            add(ip);
+                recievedData = (PackageDataSend) inputStream.readObject();
 
-            // MENSAJES
-            textArea = new JTextArea(14,24);
-            try {
-                textArea.append("YOUR IP:" + InetAddress.getLocalHost() + "\n");
-            } catch (UnknownHostException e) {
-                throw new RuntimeException(e);
-            }
-            textArea.setEditable(false);
-            add(textArea);
+                if(recievedData.getMessage().equals(" online")) {
 
-            // ENVÍO
-            message = new JTextField(20);
-            add(message);
+                    // TODO Hacer que las direcciones ip recibidas filtren la propia ip del cliente.
+                    //  Y además, muestre el nick de los demás clientes, en vez de solo la ip.
+                    ArrayList<String> connectedClients = recievedData.getConnectedClients();
 
-            sendButton = new JButton("Enviar");
+                    ip.removeAllItems();
 
-            // SALIDA DE DATOS
-            sendButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-
-                    if(!message.getText().equals(" online")) textArea.append("You >> " + message.getText() + "\n");
-
-
-                    try {
-                        // Creación de socket
-                        Socket socket = new Socket(serverIp, serverPort);
-
-                        PackageDataSend data = new PackageDataSend();
-
-                        data.setNick(nick.getText());
-                        data.setIp(ip.getSelectedItem().toString());
-                        data.setMessage(message.getText());
-
-                        ObjectOutputStream flujoSalida = new ObjectOutputStream(socket.getOutputStream());
-
-                        flujoSalida.writeObject(data);
-
-                        // TODO escribir en el chat el mensaje enviado por el propio cliente
-                        textArea.append(message.getText());
-
-                        socket.close();
-
-                    } catch (IOException ex) {
-
-                        throw new RuntimeException(ex);
-                    } finally {
-
-
-                        message.setText("");
+                    for(String ips: connectedClients) {
+                        ip.addItem(ips);
                     }
 
+                } else {
+
+                    textArea.append("\n" + recievedData.getNick() + " << " + recievedData.getMessage());
                 }
-            });
 
-            add(sendButton);
-
-            Thread mihilo = new Thread(this);
-
-            mihilo.start();
-        }
-
-        @Override
-        public void run() {
-
-            try {
-
-                ServerSocket clientListener = new ServerSocket(serverPort);
-
-                Socket cliente;
-
-                PackageDataSend recievedData;
-
-                for(;;){
-
-                    cliente = clientListener.accept();
-
-                    ObjectInputStream inputStream = new ObjectInputStream(cliente.getInputStream());
-
-                    recievedData = (PackageDataSend) inputStream.readObject();
-
-                    if(recievedData.getMessage().equals(" online")) {
-
-                        // TODO Hacer que las direcciones ip recibidas filtren la propia ip del cliente.
-                        //  Y además, muestre el nick de los demás clientes, en vez de solo la ip.
-                        ArrayList<String> connectedClients = recievedData.getConnectedClients();
-
-                        ip.removeAllItems();
-
-                        for(String ips: connectedClients) {
-                            ip.addItem(ips);
-                        }
-
-                    } else {
-
-                        textArea.append("\n" + recievedData.getNick() + " << " + recievedData.getMessage());
-                    }
-
-                }
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
             }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
-
 }
-
